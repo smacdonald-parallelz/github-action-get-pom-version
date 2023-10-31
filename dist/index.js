7665a16f90d57f2f247024b7f21c74437c8761a5
Sean Macdonald <smacdonald@parallelz.com>
2023-10-31 19:10:03 -0400

    built files need to be included

--- dist/index.js
+++ dist/index.js
@@ -4,22 +4,21 @@ const core = require("@actions/core");
 const fast_xml_parser_1 = require("fast-xml-parser");
 const promises_1 = require("fs/promises");
 const main = async () => {
+    var _a;
     const f = await (0, promises_1.readFile)(`${process.env.GITHUB_WORKSPACE}/pom.xml`, { encoding: "utf8" });
     const options = {
         ignoreAttributes: true
     };
     const parser = new fast_xml_parser_1.XMLParser(options);
-    let jsonObj = parser.parse(f);
-    if ("version" in jsonObj) {
-        return jsonObj.version;
+    let pom = parser.parse(f);
+    let version = (_a = pom === null || pom === void 0 ? void 0 : pom.project) === null || _a === void 0 ? void 0 : _a.version;
+    if (version === undefined) {
+        throw Error("could not parse version from pom.xml");
     }
-    else {
-        core.debug(jsonObj);
-        throw Error("version not found");
-    }
+    return version;
 };
 main().then(version => {
-    console.log("Successfully parsed pom.xml. Version is " + version);
+    console.info("Successfully parsed pom.xml. Version is " + version);
     core.setOutput("version", version);
 }).catch(err => {
     console.error(err);
